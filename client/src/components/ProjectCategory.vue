666927583a4fdcef06fcd9dea7fb66c5033d7908
Salman Hashmi <salman@hashmi.site>
2025-03-01 14:34:56 -0800

    Add project category background lines to project categories in home page

--- client/src/components/ProjectCategory.vue
+++ client/src/components/ProjectCategory.vue
@@ -1,6 +1,11 @@
 <template>
   <div ref="root" id="container">
     <div id="img-container">
+      <div id="dotted-lines">
+        <div id="horizontal-dotted"></div>
+        <div id="vertical-dashed"></div>
+        <div id="vertical-dotted"></div>
+      </div>
       <div id="img-background"></div>
     </div>
     <p>{{ props.projectCategory.categoryName }}</p>
@@ -15,7 +20,8 @@ const props = defineProps<{ projectCategory: ProjectCategoryModel }>()
 const root = useTemplateRef('root')
 
 onMounted(() => {
-  root.value.style.setProperty('--color', props.projectCategory.colorHash)
+  root.value.style.setProperty('--background-color', props.projectCategory.colorHash + '40')
+  root.value.style.setProperty('--dotted-color', props.projectCategory.colorHash + 'DD')
 })
 </script>
 
@@ -30,15 +36,48 @@ onMounted(() => {
   }
 
   #img-container {
+    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: center;
     height: 100px;
     width: 100px;
 
+    #dotted-lines {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      z-index: -1;
+
+      #horizontal-dotted {
+        position: absolute;
+        border-top: 1.5px dotted var(--dotted-color);
+        bottom: 25%;
+        width: 80%;
+        left: 10%;
+      }
+
+      #vertical-dashed {
+        position: absolute;
+        border-left: 1px dashed var(--dotted-color);
+        height: 70%;
+        left: 50%;
+        bottom: 0;
+        transform: translateX(-100%);
+      }
+
+      #vertical-dotted {
+        position: absolute;
+        border-left: 1.5px dotted var(--dotted-color);
+        right: 25%;
+        height: 65%;
+        bottom: 25%;
+      }
+    }
+
     #img-background {
       margin: auto;
-      background-color: var(--color);
+      background-color: var(--background-color);
       width: 80%;
       height: 80%;
       border-radius: 100%;
