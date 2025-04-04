c74625a90ac974da631b145bf7ada37fed71f7d6
Salman Hashmi <salman@hashmi.site>
2025-04-03 20:13:14 -0700

    Add banner to project page with mock data

--- client/src/assets/mockdata.ts
+++ client/src/assets/mockdata.ts
@@ -5,39 +5,6 @@ import ProjectModel from '@/models/ProjectModel'
 
 /** Temporary object representing mock data to render UI */
 export default class MockData {
-  static projects: ProjectCategoryModel[] = [
-    new ProjectCategoryModel('#F5B4B6', 'Category 1', '', [
-      new ProjectModel('Category 1 Project 1', ''),
-      new ProjectModel('Category 1 Project 2', ''),
-      new ProjectModel('Category 1 Project 3', ''),
-      new ProjectModel('Category 1 Project 4', ''),
-    ]),
-    new ProjectCategoryModel('#D0B9F5', 'Category 2', '', [
-      new ProjectModel('Category 2 Project 1', ''),
-      new ProjectModel('Category 2 Project 2', ''),
-    ]),
-    new ProjectCategoryModel('#C4E7F5', 'Category 3', '', [
-      new ProjectModel('Category 3 Project 1', ''),
-      new ProjectModel('Category 3 Project 2', ''),
-      new ProjectModel('Category 3 Project 3', ''),
-    ]),
-    new ProjectCategoryModel('#C5F5D3', 'Category 4', '', [
-      new ProjectModel('Category 4 Project 1', ''),
-      new ProjectModel('Category 4 Project 2', ''),
-      new ProjectModel('Category 4 Project 3', ''),
-      new ProjectModel('Category 4 Project 4', ''),
-      new ProjectModel('Category 4 Project 5', ''),
-      new ProjectModel('Category 4 Project 6', ''),
-      new ProjectModel('Category 4 Project 7', ''),
-    ]),
-    new ProjectCategoryModel('#D7F5C1', 'Category 5', '', [
-      new ProjectModel('Category 5 Project 1', ''),
-      new ProjectModel('Category 5 Project 2', ''),
-      new ProjectModel('Category 5 Project 3', ''),
-      new ProjectModel('Category 5 Project 4', ''),
-    ]),
-  ]
-
   static mockImageUrl = 'https://static.thenounproject.com/png/526867-200.png'
 
   static projectContent = [
@@ -61,6 +28,39 @@ export default class MockData {
     new ProjectContentModel(ProjectEntryType.LONG_IMAGE, 'Long Row title', [MockData.mockImageUrl]),
     new ProjectContentModel(ProjectEntryType.SINGLE_IMAGE, 'Single Image 2', [
       MockData.mockImageUrl,
+    ]),
+  ]
+
+  static projects: ProjectCategoryModel[] = [
+    new ProjectCategoryModel('#F5B4B6', 'Category 1', '', [
+      new ProjectModel('Category 1 Project 1', '', MockData.projectContent),
+      new ProjectModel('Category 1 Project 2', '', MockData.projectContent),
+      new ProjectModel('Category 1 Project 3', '', MockData.projectContent),
+      new ProjectModel('Category 1 Project 4', '', MockData.projectContent),
+    ]),
+    new ProjectCategoryModel('#D0B9F5', 'Category 2', '', [
+      new ProjectModel('Category 2 Project 1', '', MockData.projectContent),
+      new ProjectModel('Category 2 Project 2', '', MockData.projectContent),
+    ]),
+    new ProjectCategoryModel('#C4E7F5', 'Category 3', '', [
+      new ProjectModel('Category 3 Project 1', '', MockData.projectContent),
+      new ProjectModel('Category 3 Project 2', '', MockData.projectContent),
+      new ProjectModel('Category 3 Project 3', '', MockData.projectContent),
+    ]),
+    new ProjectCategoryModel('#C5F5D3', 'Category 4', '', [
+      new ProjectModel('Category 4 Project 1', '', MockData.projectContent),
+      new ProjectModel('Category 4 Project 2', '', MockData.projectContent),
+      new ProjectModel('Category 4 Project 3', '', MockData.projectContent),
+      new ProjectModel('Category 4 Project 4', '', MockData.projectContent),
+      new ProjectModel('Category 4 Project 5', '', MockData.projectContent),
+      new ProjectModel('Category 4 Project 6', '', MockData.projectContent),
+      new ProjectModel('Category 4 Project 7', '', MockData.projectContent),
+    ]),
+    new ProjectCategoryModel('#D7F5C1', 'Category 5', '', [
+      new ProjectModel('Category 5 Project 1', '', MockData.projectContent),
+      new ProjectModel('Category 5 Project 2', '', MockData.projectContent),
+      new ProjectModel('Category 5 Project 3', '', MockData.projectContent),
+      new ProjectModel('Category 5 Project 4', '', MockData.projectContent),
     ]),
   ]
 }
