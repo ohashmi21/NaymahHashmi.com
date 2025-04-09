78b27a9e99f60501ffd7ca925355398e5b9348a8
Salman Hashmi <salman@hashmi.site>
2025-04-08 20:52:51 -0700

    Connect to Mongo DB server

---
+++ server-ts/database-service.ts
@@ -0,0 +1,34 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import fs from "fs";
+
+const mongoUri = fs.readFileSync("./mongo-credentials.txt", "utf-8");
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(mongoUri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+function connectToDb() {
+  run().catch(console.dir);
+}
+
+export default { connectToDb };
