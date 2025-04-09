d2157f516b398366cfbdc4faa0d25cac25bca4b1
Salman Hashmi <salman@hashmi.site>
2025-04-08 20:52:51 -0700

    Connect to Mongo DB server

--- server-ts/controller.ts
+++ server-ts/controller.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import dbService from "./database-service";
+
 const app = express();
 
 app.use(express.json());
@@ -7,6 +9,8 @@ const port = 3333;
 const server = app.listen(port, () =>
   console.log("Listening on port: " + port)
 );
+
+dbService.connectToDb();
 
 app.get("/", (req: any, res: any) => {
   res.send("Hello World!");
