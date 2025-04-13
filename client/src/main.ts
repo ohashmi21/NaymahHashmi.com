aa6a8aee50039c04259a9543686e348c655b2466
Salman Hashmi <salman@hashmi.site>
2025-04-12 18:23:26 -0700

    Use projects from server instead of mock data

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { useCategories } from '@/stores/Projects'
+import { useCategories, useProjects, useAllProjectsByCategory } from '@/stores/Projects'
 
 import App from './App.vue'
 import router from './router'
 import CategoriesService from './services/CategoriesService'
+import ProjectsService from './services/ProjectsService'
+import type ProjectCategoryModel from './models/ProjectCategoryModel'
+import type ProjectModel from './models/ProjectModel'
 
 const app = createApp(App)
 
@@ -14,4 +17,21 @@ app.use(router)
 app.mount('#app')
 
 const categoriesStore = useCategories()
+const projectsStore = useProjects()
+const allProjectsByCategoryStore = useAllProjectsByCategory()
+
 CategoriesService.getCategories().then((res) => (categoriesStore.categories = res))
+
+ProjectsService.getProjects().then((res: ProjectCategoryModel[]) => {
+  res.forEach((category: ProjectCategoryModel) => {
+    category.projects.forEach((project) => {
+      const processedProjects: Map<string, ProjectModel> = new Map()
+      processedProjects.set(project.projectName, project)
+      projectsStore.projects = processedProjects
+    })
+  })
+
+  allProjectsByCategoryStore.allProjectsByCategory = res
+
+  console.log(res)
+})
--- client/src/services/ProjectsService.ts
+++ client/src/services/ProjectsService.ts
@@ -1,3 +1,4 @@
+import type ProjectCategoryModel from '@/models/ProjectCategoryModel'
 import ProjectModel from '@/models/ProjectModel'
 import axios from 'axios'
 
@@ -7,6 +8,12 @@ export default class ProjectsService {
     axios.post('http://localhost:3333/project', {
       projectCategory: categoryId,
       projectModel: project,
+    })
+  }
+
+  static getProjects(): Promise<ProjectCategoryModel[]> {
+    return axios.get('http://localhost:3333/all-projects').then((res) => {
+      return res.data as ProjectCategoryModel[]
     })
   }
 }
